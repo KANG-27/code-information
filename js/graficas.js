355f3400692d5df5874532274143319dcc459d9e
Josse122 <jose27alejo@hotmail.com>
2022-05-09 16:58:22 -0500

    Vista de graficas ajustada

--- js/graficas.js
+++ js/graficas.js
@@ -1,3 +1,74 @@
+var app = new (function () {
+    this.el = document.getElementById("tasks");
+    this.tasks = [];
+  
+    this.FetchAll = function () {
+      var data = "";
+  
+      if (this.tasks.length > 0) {
+        for (i = 0; i < this.tasks.length; i++) {
+          data += "<tr>";
+          data += "<td>" + (i + 1) + ". " + this.tasks[i] + "</td>";
+          data += '<td><button onclick="app.Edit(' + i + ')">Editar</button></td> ';
+          data +=
+            '<td><button onclick="app.Delete(' + i + ')">Eliminar</button></td> ';
+          data += "</tr>";
+        }
+      }
+      this.Count(this.tasks.length);
+      return (this.el.innerHTML = data);
+    };
+  
+    this.Add = function () {
+      el = document.getElementById("add-todo");
+      var task = el.value;
+      if (task) {
+        this.tasks.push(task.trim());
+        this.el.value = "";
+        this.FetchAll();
+      }
+    };
+  
+    this.Edit = function (item) {
+      el = document.getElementById("edit-todo");
+      el.value = this.tasks[item];
+      document.getElementById("edit-box").style.display = "block";
+      self = this;
+  
+      document.getElementById("save-edit").onsubmit = function () {
+        var task = el.value;
+        if (task) {
+          self.tasks.splice(item, 1, task.trim());
+          self.FetchAll();
+          CloseInput();
+        }
+      };
+    };
+  
+    this.Delete = function (item) {
+      this.tasks.splice(item, 1);
+      this.FetchAll();
+    };
+  
+    this.Count = function (data) {
+      var el = document.getElementById("counter");
+      var name = "Tareas";
+      if (data) {
+        if (data == 1) {
+          name = "Tareas";
+        }
+        el.innerHTML = data + " " + name;
+      } else {
+        el.innerHTML = " " + name;
+      }
+    };
+  })();
+  app.FetchAll();
+  
+  function CloseInput() {
+    document.getElementById("edit-box").style.display = "none";
+  }
+
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'doughnut',
